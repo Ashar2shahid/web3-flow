2a054071ac388a7e20d0928acc25ea79fea001fb
beejsbj <burooj.bj@gmail.com>
2024-12-07 14:33:58 -0500

    modify start node to use base node

--- src/components/Workflow/nodes/Start/StartNode.tsx
+++ src/components/Workflow/nodes/Start/StartNode.tsx
@@ -1,9 +1,8 @@
 import React, { memo } from "react";
-import { Handle, Position, NodeProps } from "reactflow";
+import { NodeProps } from "reactflow";
 import { Clock, Webhook, Blocks } from "lucide-react";
-import { handleStyles, nodeStyles } from "../../styles/nodeStyles";
 import { TriggerType } from "../../../../types/workflow";
-import { useWorkflowStore } from "../../../../stores/workflowStore";
+import BaseNode from "../Base/BaseNode";
 
 const iconMap: Record<TriggerType, React.ElementType> = {
   cronjob: Clock,
@@ -11,37 +10,18 @@ const iconMap: Record<TriggerType, React.ElementType> = {
   onchain: Blocks,
 };
 
-function StartNode({ data, id }: NodeProps) {
+function StartNode({ data, ...props }: NodeProps) {
   const trigger = data.trigger;
   const Icon = trigger ? iconMap[trigger.type] : Clock;
-  const isCompleted = useWorkflowStore((state) => state.completedNodes.has(id));
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="relative">
-        <Handle
-          type="source"
-          position={Position.Right}
-          className={handleStyles.source}
-        />
-        <div
-          className={`${nodeStyles.startNode} transition-colors ${
-            isCompleted
-              ? "border-green-500 shadow-[0_0_0_1px_rgba(34,197,94,0.5)]"
-              : "border-gray-200"
-          }`}
-        >
-          <Icon
-            className={`h-6 w-6 ${
-              isCompleted ? "text-green-500" : "text-primary-500"
-            }`}
-          />
-        </div>
-      </div>
-      <div className="mt-1 text-xs text-gray-600 text-center max-w-[100px] truncate">
-        {data.label || "Start"}
-      </div>
-    </div>
+    <BaseNode
+      data={data}
+      icon={Icon}
+      outputs={[{ id: "default", label: "", type: "output" }]}
+      inputs={[]}
+      {...props}
+    />
   );
 }
 
